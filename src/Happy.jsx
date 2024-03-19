89fad56fa1ddf18f721303d486a43499e12e8f7f
AS-EBaiden <eunice.baiden@alliedsolutions.net>
2024-03-19 12:23:46 -0400

    switching to happy component

---
+++ src/Happy.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Happy() {
+  return <h1>Happy</h1>;
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
-import Sample from "./Sample";
+// import App from "./App";
+// import Sample from "./Sample";
+import Happy from "./Happy";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Happy />
     {/* <Sample /> */}
   </StrictMode>
 );
