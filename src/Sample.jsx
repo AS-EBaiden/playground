85325580dc8e96f15b82e1005b8f60ea928644da
AS-EBaiden <eunice.baiden@alliedsolutions.net>
2024-03-19 19:11:19 -0400

    formatting

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-
-import * as bob from "@allied-solutions/affinity";
 import { AffinityProvider, GlobalStyle } from "@allied-solutions/affinity";
 import "./styles.css";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
@@ -36,50 +34,50 @@ export default function Sample() {
   // `;
 
   const code = `
-() => {
-    const variable="Project"
-    return(
-        <Container>
-    <Box        
-    borderRadius={4}
-    gradient="heroBlue"
-    typeStyle="displayLarge"
-    textAlign="center"
-    color="white">Your Starter {variable}</Box>
-    <Formik
-    initialValues={{
-      input: "",
-    }}
-    onSubmit={async () => {
-      await sleep(2000);
-      alert("submitted");
-    }}
-  >
-    {(formProps) => (
-      <Form>
-        <FormGroup id="input" bg="muted" p={4} borderRadius={1}>
-          <FormGroup.Label>Label</FormGroup.Label>
-          <FormGroup.InputGroup borderRadius="full">
-            <FormGroup.InputGroup.Input placeholder="I am a text input" />
-          </FormGroup.InputGroup>
-          <FormGroup.Caption />
-        </FormGroup>
-
-        {/* end form group */}
-
-        <SubmitButton
-          id="SubmitButton--test"
-          formikProps={formProps}
-          rounded
-        />
-
-        <pre>{JSON.stringify(formProps.values, null, 2)}</pre>
-      </Form>
-    )}
-  </Formik>
-    </Container >
-  )}
-`;
+  () => {
+    const variable = "Project";
+    return (
+      <Container>
+        <Box
+          borderRadius={4}
+          gradient="heroBlue"
+          typeStyle="displayLarge"
+          textAlign="center"
+          color="white"
+        >
+          Your Starter {variable}
+        </Box>
+        <Formik
+          initialValues={{
+            input: "",
+          }}
+          onSubmit={async () => {
+            await sleep(2000);
+            alert("submitted");
+          }}
+        >
+          {(formProps) => (
+            <Form>
+              <FormGroup id="input" bg="muted" p={4} borderRadius={1}>
+                <FormGroup.Label>Label</FormGroup.Label>
+                <FormGroup.InputGroup borderRadius="full">
+                  <FormGroup.InputGroup.Input placeholder="I am a text input" />
+                </FormGroup.InputGroup>
+                <FormGroup.Caption />
+              </FormGroup>
+              {/* end form group */}
+              <SubmitButton
+                id="SubmitButton--test"
+                formikProps={formProps}
+                rounded
+              />
+              <pre>{JSON.stringify(formProps.values, null, 2)}</pre>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    );
+  }`;
 
   return (
     <div style={{ display: "inline-flex", width: "100%" }}>
