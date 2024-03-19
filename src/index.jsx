7b125959c226bc3b3d954a633d78e6271d0f370a
AS-EBaiden <eunice.baiden@alliedsolutions.net>
2024-03-19 16:21:16 -0400

    trying to get it up and running

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
+// import App from "./App";
 import Sample from "./Sample";
 
 const rootElement = document.getElementById("root");
