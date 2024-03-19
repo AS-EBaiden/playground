8fbce9fdd0ee70434aa3d72ec8ffed75c087464f
AS-EBaiden <eunice.baiden@alliedsolutions.net>
2024-03-19 15:59:17 -0400

    playground switched

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-// import App from "./App";
-// import Sample from "./Sample";
-import Happy from "./Happy";
+import App from "./App";
+import Sample from "./Sample";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -11,7 +10,6 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     {/* <App /> */}
-    <Happy />
-    {/* <Sample /> */}
+    <Sample />
   </StrictMode>
 );
