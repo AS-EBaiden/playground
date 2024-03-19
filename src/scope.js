8cbcdd12e2716e59994b1bba805d17a8138b647f
AS-EBaiden <eunice.baiden@alliedsolutions.net>
2024-03-19 16:59:22 -0400

    reducer method

--- src/scope.js
+++ src/scope.js
@@ -1,4 +1,13 @@
 import * as affinityComponents from "@allied-solutions/affinity";
-export const scope = {
-  ...affinityComponents,
-};
+// export const scope = {
+//   affinityComponents,
+// };
+export const scope = Object.entries(affinityComponents).reduce(
+  (acc, [key, value]) => {
+    return {
+      ...acc,
+      [key]: value,
+    };
+  },
+  {}
+);
